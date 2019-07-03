c1bcf5647b1ca2082ebc89950f016dfb2d9656b2
Yuqi Liu <yliu176@stevens.edu>
2019-07-03 11:41:13 -0400

    Readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Record: <br>
 
 | # | Title | Source Code | Article | Difficulty | Finish Date |
 |:---:|:---:|:---:|:---:|:---:|:---:|
-|1|[two-sum](https://leetcode.com/problems/two-sum)|[Python](https://github.com/iamyuqi/LeetCode/blob/master/Solutions/001_two_sum.py)|[:bookmark_tabs:](https://leetcode.com/articles/two-sum/)|Easy|
+|1|[two-sum](https://leetcode.com/problems/two-sum)|[Python](https://github.com/iamyuqi/LeetCode/blob/master/Solutions/001_two_sum.py)|[:bookmark_tabs:](https://leetcode.com/articles/two-sum/)|Easy|July 3, 2019
---
+++ Solutions/001_two_sum.py
@@ -0,0 +1,23 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+#Example:
+#Given nums = [2, 7, 11, 15], target = 9,
+#Because nums[0] + nums[1] = 2 + 7 = 9,
+#return [0, 1].
+
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            r = target - nums[i]
+            if r in nums[i+1:]:
+                ind = nums[i+1:].index(r)+i+1
+                index = [i, ind]
+                break
+        return index
+
+t = Solution()
+t.twoSum([3,3], 6)
+
+
+
